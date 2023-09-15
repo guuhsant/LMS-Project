ba8e86c45d6ee7e956b37f0073bd2f43c5b683bd
Gustavo de Castro Santos <117998692+guuhsant@users.noreply.github.com>
2023-09-15 16:55:17 -0300

    Delete LMS-Projeto directory

--- LMS-Projeto/navbar.js
+++
@@ -1,6 +0,0 @@
-const btnMobile = document.getElementById("btn-mobile");
-      const headerNavbar = document.querySelector(".header-navbar");
-
-      btnMobile.addEventListener("click", ()=>{
-  headerNavbar.classList.toggle("active");
- }) 
